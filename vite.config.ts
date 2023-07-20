d2b68e5b7c5f9f86c9835a79c4842826ec281f70
AEMatamoros <alexismatamoros1996888@gmail.com>
2023-07-19 22:14:03 -0600

    Updated base url on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 export default defineConfig({
-  base: "/PorfolioV2/",
+  base: "/",
   plugins: [react(), pluginRewriteAll()],
 });
