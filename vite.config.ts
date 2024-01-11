92fadc922d0508b5c2954b5f0330db0e45c98b87
AEMatamoros <49670332+AEMatamoros@users.noreply.github.com>
2024-01-11 14:15:04 -0600

    change: Removed css purger conflict with theme change css variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
-import pluginPurgeCss from '@mojojoejo/vite-plugin-purgecss';
 
 export default defineConfig({
     base: '/PorfolioV2/',
-    plugins: [react(), pluginRewriteAll(), pluginPurgeCss({})],
+    plugins: [react(), pluginRewriteAll()],
 });
