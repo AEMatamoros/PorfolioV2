0e69b583bd1e9a8a7338151713f21aa4ab34a828
AEMatamoros <49670332+AEMatamoros@users.noreply.github.com>
2024-01-11 14:08:50 -0600

    feat: Added css purge

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import pluginRewriteAll from "vite-plugin-rewrite-all";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
+import pluginPurgeCss from '@mojojoejo/vite-plugin-purgecss';
 
 export default defineConfig({
-  base: "/PorfolioV2/",
-  plugins: [react(), pluginRewriteAll()],
+    base: '/PorfolioV2/',
+    plugins: [
+        react(),
+        pluginRewriteAll(),
+        pluginPurgeCss({
+            variables: true,
+        }),
+    ],
 });
