953671bd6794c0c6ffc83c9b2da9d5564721b6c8
AEMatamoros <alexismatamoros1996888@gmail.com>
2024-02-09 14:50:23 +0000

    Updates

---
+++ assets/NotFound.e619d7ed.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.be69524c.js";function n(){return e("section",{className:"main__404 bg-main flex justify-center items-center",children:e("h1",{className:"text-white text-4xl",children:"Opps....\u{1F92F}"})})}export{n as default};
